16447aeef96e8535bd818bf06161412f853af2f5
Umerkhan26 <114725165+Umerkhan26@users.noreply.github.com>
2025-08-19 03:39:04 +0500

    updated

--- src/routes/bankPremium.routes.ts
+++ src/routes/bankPremium.routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   createBankPremium,
   deleteBankPremium,
@@ -6,20 +6,24 @@ import {
   getAllBankPremiums,
   getBankPremiumById,
   verifyBankPremiumCode,
-  redeemBankPremium
-} from '../controllers/bankPremiumController';
-import { upload } from '../middlewares/multer';
+  redeemBankPremium,
+} from "../controllers/bankPremiumController";
+import { upload } from "../middlewares/multer";
 
 const router = express.Router();
 
-router.post('/bankPremiums', upload.single('image'), createBankPremium);
-router.put('/updateBankPremiums/:bankPremiumId', upload.single('image'), updateBankPremium);
-router.delete('/deleteBankPremiums/:bankPremiumId', deleteBankPremium);
-router.get('/getBankPremiums', getAllBankPremiums);
-router.get('/getBankPremiumById/:bankPremiumId', getBankPremiumById);
+router.post("/bankPremiums", upload.single("image"), createBankPremium);
+router.put(
+  "/updateBankPremiums/:bankPremiumId",
+  upload.single("image"),
+  updateBankPremium
+);
+router.delete("/deleteBankPremiums/:bankPremiumId", deleteBankPremium);
+router.get("/getBankPremiums", getAllBankPremiums);
+router.get("/getBankPremiumById/:bankPremiumId", getBankPremiumById);
 // User redeem endpoint
-router.post('/redeemBankPremium:premiumId/redeem', redeemBankPremium);
+router.post("/redeemBankPremium/:premiumId/redeem", redeemBankPremium);
 
 // Admin verify/update status endpoint
-router.put('/verifyCodeAndUpdateStatus/:code', verifyBankPremiumCode);
+router.put("/verifyCodeAndUpdateStatus/:code", verifyBankPremiumCode);
 export default router;
